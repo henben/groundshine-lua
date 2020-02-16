461f86e93368c335a484e912ef3971fad55b2636
henben <ben.henley@gmail.com>
2020-02-16 16:54:43 +0000

    Randomise target heights

--- Bomber.lua
+++ Bomber.lua
@@ -1,8 +1,9 @@
 Bomber = Object:extend()
 
 function Bomber:new()
+    self.random_height = love.math.random(gh/8, gh/2)
     self.x = 0
-    self.y = (gh - 0.75*gh)
+    self.y = (self.random_height)
     self.w = gw/20
     self.h = gh/20
     self.collider = world:newRectangleCollider(self.x, self.y, self.w, self.h)
@@ -19,6 +20,7 @@ end
 
 function Bomber:update()
     self.x, self.y = self.collider:getPosition()
+    self.collider:setLinearVelocity(gw/50, 0)
     if self.collider:enter('Explosion') or self.x > gw then
         self.collider:destroy()
         self.dead = true
